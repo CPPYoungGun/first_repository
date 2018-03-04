72788b1ce8ba772f69be7b93dd6bb958b7a0454c
MGaida <encrypted@serwer.com>
2018-03-04 19:46:23 +0100

    V 2.5 New field inside class THello

---
+++ new-feature.cpp
@@ -0,0 +1,41 @@
+#include <iostream> 
+
+class THello {
+
+public:
+
+	/*static void Hello()
+	{
+	std::cout << "Hello world\n";
+	}
+	*/
+
+	void Hello()
+	{
+		std::cout << "Hello world\n";
+		counter += 2 ; 
+	}
+
+	void getCounter() const
+	{
+		std::cout << counter << std::endl; 
+	}
+
+private:
+	static int counter;
+
+};
+
+ int THello::counter = 0;
+
+
+int main()
+{
+	//std::cout << "Hello world\n"; 
+	//THello::Hello();
+	THello ob1;
+	ob1.Hello();
+	ob1.getCounter();
+
+	return 0;
+}
