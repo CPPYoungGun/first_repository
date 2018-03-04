b3402305a10113a21de623761a1bd6057c729263
MGaida <encrypted@serwer.com>
2018-03-04 19:27:41 +0100

    V 1.0 OOP

--- program.cpp
+++ program.cpp
@@ -1,21 +1,22 @@
 #include <iostream> 
 
+class THello{
+	
+public:
+static void Hello()
+{
+	std::cout << "Hello world\n";
+}	
+	
+	
+	
+};
+
 int main()
 {
-	std::cout << "Hello world\n"; 
+	//std::cout << "Hello world\n"; 
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	THello::Hello();
 	
 	return 0; 
 }
